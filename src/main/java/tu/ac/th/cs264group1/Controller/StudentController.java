5a2b238c6d3e3374da1f5855c7bb5cea6f12d613
Natthachak <natthachak.jeu@dome.tu.ac.th>
2023-11-15 22:39:01 +0700

    Add file in java and resources folder

---
+++ src/main/java/tu/ac/th/cs264group1/Controller/StudentController.java
@@ -0,0 +1,4 @@
+package tu.ac.th.cs264group1.Controller;
+
+public class StudentController {
+}
---
+++ src/test/java/tu/ac/th/cs264group1/Cs264Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tu.ac.th.cs264group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs264Group1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
