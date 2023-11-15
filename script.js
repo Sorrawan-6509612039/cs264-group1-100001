99810c1005c4ea427b58ffdae76c791f341ad3bb
นันท์นภัส <pam@Macintosh.local>
2023-11-15 19:07:07 +0700

    Check the message to match the format.

--- script.js
+++ script.js
@@ -22,5 +22,44 @@ function changePage(values) {
        default:
        return;
         }
-            window.location.href = fileName;
-        }
+        window.location.href = fileName;
+   }
+
+   function redirectToStartPage() {
+        window.location.href = "pagestart.html";
+   }
+
+   function validateForm() {
+       var studentID = document.getElementById("studentId").value;
+       var mobilePhone = document.getElementById("mobilePhone").value;
+       var postalcode = document.getElementById("postalcode").value;
+
+
+
+       if (studentID.length !== 10 || isNaN(studentID)) {
+           alert("กรุณากรอกรหัสนักศึกษาเป็นตัวเลข 10 หลัก");
+           return false;
+       }
+
+       if (postalcode.length !== 5 || isNaN(postalcode)) {
+           alert("กรุณากรอกรหัสไปรษณีย์เป็นตัวเลข 5 หลัก");
+           return false;
+       }
+
+       if (mobilePhone.length !== 10 || isNaN(mobilePhone)) {
+           alert("กรุณากรอกเบอร์โทรศัพท์มือถือเป็นตัวเลข 10 หลัก");
+           return false;
+       }
+
+
+
+       finishSubmit();
+       return true;
+   }
+
+
+    function finishSubmit() {
+        window.location.href = "finish.html";
+        alert("Form submitted successfully!");
+    }
+
